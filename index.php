2fcfee1708b45cd33cbd5d5652be0709a7b7ba99
Furkan Ayyıldız <furkan199905@gmail.com>
2022-01-04 13:57:32 +0300

    deleting and update part added

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php 
+require_once "baglan.php"; 
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,15 +15,18 @@
         <h1>Veritabanı PDO kayıt işlemleri</h1>
         <hr>
         <?php 
-        
-        if($_GET['durum']== "ok"){
-            echo "işlem başarılı";
+	if ($_GET['durum']=="ok") {
+		
+		echo "İşlem başarılı";
 
-        }elseif ($_GET["durum"] == "no"){
-            echo "işlem başarısız";
-        }
-        
-        ?>
+	} elseif ($_GET['durum']=="no") {
+
+		echo "İşlem başarısız";
+
+
+	}
+
+	?>
         <form action="islem.php" method= "POST">
 
         <input type="text" required ="" name="bilgilerim_ad" >
@@ -28,6 +36,49 @@
 
         <button type= "submit" name="insertislemi">Formu Kaydet</button>
         </form>
+        <br>
+
+	    <h4>Kayıtların Listelenmesi</h4>
+	    <hr>
+       
+    <table style= "width: 60%" border="1">
+        <tr>
+            <th>Sıra No</th>
+            <th>ID</th>
+            <th>Ad</th>
+            <th>Soyad</th>
+            <th>Mail</th>
+            <th>Yaş</th>
+            <th width = "50">İşlemler</th>
+            <th width = "50">İşlemler</th>
+        </tr>
+        <?php 
+
+            //Select İşlemi
+
+            $bilgilerimsor=$db->prepare("SELECT * from bilgilerim");
+            $bilgilerimsor->execute();
+            $say =0;
+            while($bilgilerimcek=$bilgilerimsor->fetch(PDO::FETCH_ASSOC)) { $say++;?>
+            
+        <tr>
+            <td><?php echo $say?></td>
+            <td><?php echo $bilgilerimcek["bilgilerim_id"]?></td>
+            <td><?php echo $bilgilerimcek["bilgilerim_ad"]?></td>
+            <td><?php echo $bilgilerimcek["bilgilerim_soyad"]?></td>
+            <td><?php echo $bilgilerimcek["bilgilerim_mail"]?></td>
+            <td><?php echo $bilgilerimcek["bilgilerim_yas"]?></td>
+            <td align="center"><a href="duzenle.php?bilgilerim_id=<?php echo $bilgilerimcek["bilgilerim_id"]?>"><button>Düzenle</button></a></td>
+            <td align="center"><a href="islem.php?bilgilerim_id=<?php echo $bilgilerimcek["bilgilerim_id"]?>&bilgilerimsil=ok"><button>Sil</button></a></td>
+        </tr>
+        <?php } ?>
+    </table>
+
+
+     
+
+
+
 
 </body>
 </html>
